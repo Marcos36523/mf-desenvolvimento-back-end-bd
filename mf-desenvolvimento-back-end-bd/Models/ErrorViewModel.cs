40d9c1b3dc75f67e008009d68b5e084887a5e095
Marcos36523 <marcos.dias.1457746@sga.pucminas.br>
2023-10-02 17:10:55 -0300

    Adicionar arquivos de projeto.

---
+++ mf-desenvolvimento-back-end-bd/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace mf_desenvolvimento_back_end_bd.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
